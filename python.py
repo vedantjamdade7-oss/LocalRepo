041da6edd8c508cdaf96462f4f31ad5566f851d9
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-01-09 22:30:22 +0530

    Add Inicial File

--- python.py
+++ python.py
@@ -1,28 +1,9 @@
-# Simple Calculator
+# Variable 
 
+a = "Hello, World!" # a is a variable that store the string "Hello, World!"
+print(a)
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-sum = input("Choose an operation +, *, -, / : ")
-
-if sum == "+":
-    print("Result:", num1 + num2)
-
-elif sum == "*":
-    print("Result:", num1 * num2)
-
-elif sum == "-":
-    print("Result:", num1 - num2)
-
-elif sum == "/":
-    if num2 != 0:
-        print("Result:", num1 / num2)
-    else:
-        print("Error: Cannot divide by zero")
-
-else:
-    print("Invalid choice")
+# variable means a reserved memeory location to store values. 
 
 
 
